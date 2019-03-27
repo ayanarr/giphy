6883bdf025e34043b72bae2b19bc3158def655bf
ayana <ayanaroman84@gmail.com>
2019-03-27 22:08:07 +0000

    java

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
-});
-
+    var searchTerm = $("input").val();
+    var index =Math.floor((Math.random() * 28) );
+    function randomInt(max){
+    }
+    $.ajax({
+        url:  "https://api.giphy.com/v1/gifs/search?q="+searchTerm+"&rating=pg&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+       success: function(response) {
+       $(".gallery").html("<img src=" +response.data[index].images.fixed_width.url+ ">")
+    }
+    
+    })
+       });
